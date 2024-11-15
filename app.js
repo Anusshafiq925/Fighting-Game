e77fc75b3d75468df1d0c9af9fe11665116fdd54
Anus Shafiq <sheikhanus925@gmail.com>
2024-11-15 23:26:22 +0500

    Update Code of Getting User Info

--- app.js
+++ app.js
@@ -1,5 +1,51 @@
 var playerOneRun = 0;
 var playerTwoRun = 0;
+var playerOneName = "";
+var playerTwoName = "";
+
+// start game
+
+function displayNone() {
+  document.getElementById("game").style.display = "none";
+}
+displayNone();
+
+function startGame() {
+  document.getElementById("list").style.display = "block";
+  document.getElementById("playersName").style.display = "none";
+  document.getElementById("controls").style.display = "none";
+}
+
+function controls() {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("playersName").style.display = "none";
+  document.getElementById("controls").style.display = "block";
+}
+
+function playersName() {
+  document.getElementById("list").style.display = "none";
+  document.getElementById("playersName").style.display = "block";
+  document.getElementById("controls").style.display = "none";
+}
+startGame();
+
+// getting name
+
+function submit() {
+  playerOneName = document.getElementById("firstName").value;
+  playerTwoName = document.getElementById("secondName").value;
+  document.getElementById("firstPlayerName").innerHTML = `${playerOneName}`;
+  document.getElementById("secondPlayerName").innerHTML = `${playerTwoName}`;
+
+  if (playerOneName !== "" && playerTwoName !== "") {
+    document.getElementById("gettingUserInfo").style.display = "none";
+    document.getElementById("game").style.display = "block";
+  } else {
+    Swal.fire("Please Enter Players Name");
+  }
+
+  startTimer();
+}
 
 function playerOneKeys() {
   Swal.fire({
@@ -41,6 +87,25 @@ function playerTwoKeys() {
     },
   });
 }
+
+// timer
+
+var timeLeft = 30;
+var timerDisplay = document.getElementById("time");
+
+function startTimer() {
+  var timeStart = setInterval(() => {
+    timeLeft--;
+    timerDisplay.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+      clearInterval(timeStart);
+      var userShow = Swal.fire("Time Over");
+    }
+  }, 1000);
+}
+
+// keyCode
 
 window.onkeydown = function () {
   // Player-One-Code-Start
